a5a10816cbd7d4f21caf30e6710764922e3a843a
zx <2466564159@qq.com>
2023-12-21 20:28:05 +0800

    扫描 scanning

---
+++ java/test/test.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHello() {
+        console.log(`hello my name is ${this.name}, i'm ${this.age} years old`);
+    }
+}
